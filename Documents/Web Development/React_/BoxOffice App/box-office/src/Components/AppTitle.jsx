523590a041f5bb883f487656ba49ccae93369de0
Parteek Sandhu <parteek.sandhu@s.amity.edu>
2023-10-13 19:24:57 +0530

    added navigation for home and starred page

---
+++ Documents/Web Development/React_/BoxOffice App/box-office/src/Components/AppTitle.jsx
@@ -0,0 +1,13 @@
+export default function AppTitle(props) {
+  const {
+    title = "BoxOffic App",
+    subTitle = "Are you looking for an actor or a movie...",
+  } = props;
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h1>{subTitle}</h1>
+    </div>
+  );
+}
--- Documents/Web Development/React_/BoxOffice App/box-office/src/Pages/Home.jsx
+++ Documents/Web Development/React_/BoxOffice App/box-office/src/Pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div>Home Page</div>;
+  return <div>Home Page...</div>;
 }
 
 export default Home;
