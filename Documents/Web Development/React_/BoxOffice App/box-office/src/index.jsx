5fe1c58abe3ffa46457c7b6ff11e81e375538bea
Parteek Sandhu <parteek.sandhu@s.amity.edu>
2023-10-14 23:14:01 +0530

    used useQuery on Home page to fetch search data

--- Documents/Web Development/React_/BoxOffice App/box-office/src/index.jsx
+++ Documents/Web Development/React_/BoxOffice App/box-office/src/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,10 +7,11 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
 );
 
 // If you want your app to work offline and load faster, you can change
