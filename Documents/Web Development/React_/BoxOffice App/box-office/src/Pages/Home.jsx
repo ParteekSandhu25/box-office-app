e7f9e181703f20fb88ca4dd0a001aaf5cf512fc2
Parteek Sandhu <parteek.sandhu@s.amity.edu>
2023-10-14 16:42:15 +0530

    search for shows on button click

--- Documents/Web Development/React_/BoxOffice App/box-office/src/Pages/Home.jsx
+++ Documents/Web Development/React_/BoxOffice App/box-office/src/Pages/Home.jsx
@@ -1,8 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
 function Home() {
-  return <div>Home Page...</div>;
+  const [searchStr, setsearchStr] = useState("");
+
+  const searchStrHandler = (event) => {
+    setsearchStr(event.target.value);
+  };
+
+  const onSearch = async (event) => {
+    event.preventDefault();
+
+    const res = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchStr}`
+    );
+    const body = await res.json();
+
+    console.log(body);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" onChange={searchStrHandler} value={searchStr} />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 }
 
 export default Home;
